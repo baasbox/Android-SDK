e7f8a15ffc34ad20997214e893593df18dde478b
aKtor <eliantor@gmail.com>
2014-01-15 11:19:32 +0100

    base object

--- src/com/baasbox/android/BAASObject.java
+++ src/com/baasbox/android/BAASObject.java
@@ -8,32 +8,16 @@ import com.baasbox.android.spi.HttpRequest;
 
 import org.apache.http.HttpResponse;
 
+
 /**
- * Created by eto on 13/01/14.
+ * Base class for remote objects
+ * Created by Andrea Tortorella on 13/01/14.
  */
 public abstract class BAASObject<E extends BAASObject<E>> {
+    // todo this should provide common interface among remote objects
+    //      such as dirty tracking timestamps ecc
 
-
-    public abstract <T> RequestToken save(BAASBox client, T tag, Priority priority, BAASBox.BAASHandler<E, T> handler);
-
-    public RequestToken save(BAASBox client, BAASBox.BAASHandler<E, ?> handler) {
-        return save(client, null, Priority.NORMAL, handler);
-    }
-
-    public <T> RequestToken save(T tag, Priority priority, BAASBox.BAASHandler<E, T> handler) {
-        BAASBox box = BAASBox.getDefaultChecked();
-        return save(box, tag, priority, handler);
-    }
-
-    public RequestToken save(BAASBox.BAASHandler<E, ?> handler) {
-        BAASBox box = BAASBox.getDefaultChecked();
-        return save(box, null, Priority.NORMAL, handler);
-    }
-
-    public abstract BaasResult<E> saveSync(BAASBox client);
-
-    public BaasResult<E> saveSync() {
-        return saveSync(BAASBox.getDefaultChecked());
+    BAASObject() {
     }
 
 
